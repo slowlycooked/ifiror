e1e0d4e2e21a6a893d79bb3a8b8bdb3f63785b68
slowlycooked <flyhigh921@hotmail.com>
2016-02-26 09:34:52 +0800

    redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   private
   def set_year
     if session[:current_year] == nil
