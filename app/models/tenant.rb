6d93bf79b3ae797137026350a508a6eb9478e781
slowlycooked <flyhigh921@hotmail.com>
2016-03-08 13:22:05 +0800

    sign up

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,7 +1,7 @@
 class Tenant < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable, :timeoutable #, :registerable, :recoverable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable, :timeoutable, :registerable, :recoverable
 
   has_many :customers
   has_many :books
